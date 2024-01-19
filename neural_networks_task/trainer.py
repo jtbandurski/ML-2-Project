0e07699c4dfc17aa4f296ba21ff7d89422f4c653
Dominik Koterwa <koterwadominik@icloud.com>
2024-01-19 16:04:15 +0100

    add metrics logging and dataset splitting

--- neural_networks_task/trainer.py
+++ neural_networks_task/trainer.py
@@ -1,7 +1,7 @@
 from utils import loss_fn, CONFIG
 from tqdm import tqdm
 import torch
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, accuracy_score
 from datasets import BirdCLEFDataset
 from torch.utils.data import DataLoader
 
@@ -17,9 +17,11 @@ class Trainer:
         self.model = model
         self.device = device
 
-    def train(self, dataloader, optimizer, scheduler, device, epoch, train_one_batch=False):
+    def train(self, dataloader, optimizer, scheduler, device, epoch):
         self.model.train()
         total_loss = 0
+        labels_list = []
+        preds_list = []
         loop = tqdm(dataloader, position=0)
         for i, (mels, labels) in enumerate(loop):
             mels = mels.to(device)
@@ -33,36 +35,38 @@ class Trainer:
             if scheduler is not None:
                 scheduler.step()
             total_loss += loss.item()
-            loop.set_description(f"Epoch [{epoch+1}/{CONFIG['epochs']}]")
+            labels_list.extend(labels.cpu().numpy())
+            preds_list.extend(preds.cpu().numpy())
+            loop.set_description(f"Training epoch [{epoch+1}/{CONFIG['epochs']}]")
             loop.set_postfix(loss=loss.item())
 
-        return total_loss / len(dataloader)
+        accuracy = accuracy_score(labels.cpu().numpy(), preds.cpu().numpy())
+        f1 = f1_score(labels.cpu().numpy(), preds.cpu().numpy(), average="macro")
+        return total_loss / len(dataloader), accuracy, f1
 
     def eval(self, dataloader, device, epoch):
         self.model.eval()
         total_loss = 0
-        pred = []
-        label = []
+        preds_list = []
+        labels_list = []
 
         loop = tqdm(dataloader, position=0)
-        for mels, labels in tqdm(dataloader):
-            print(f"Mels shape {mels.shape()}")
-            mels = mels.to(device)
-            labels = labels.to(device)
-            print(f"Labels shape {labels.shape()}")
-            outputs = self.model(mels)
-            print(f"Outputs shape {outputs.shape()}")
-            _, preds = torch.max(outputs, 1)
-            loss = loss_fn(outputs, labels)
-            total_loss += loss.item()
-            pred.extend(preds.view(-1).cpu().detach().numpy())
-            label.extend(labels.view(-1).cpu().detach().numpy())
-            loop.set_description(f"Epoch [{epoch+1}/{CONFIG['epochs']}]")
-            loop.set_postfix(loss=loss.item())
+        with torch.no_grad():
+            for mels, labels in tqdm(dataloader):
+                mels = mels.to(device)
+                labels = labels.to(device)
+                outputs = self.model(mels)
+                _, preds = torch.max(outputs, 1)
+                loss = loss_fn(outputs, labels)
+                total_loss += loss.item()
+                preds_list.extend(preds.view(-1).cpu().detach().numpy())
+                labels_list.extend(labels.view(-1).cpu().detach().numpy())
+                loop.set_description(f"Evaluation epoch [{epoch+1}/{CONFIG['epochs']}]")
+                loop.set_postfix(val_loss=loss.item())
 
-            valid_f1 = f1_score(label, pred, average="macro")
-
-            return total_loss / len(dataloader), valid_f1
+        accuracy = accuracy_score(labels_list, preds_list)
+        f1 = f1_score(labels_list, preds_list, average="macro")
+        return total_loss / len(dataloader), accuracy, f1
 
     def overfit_single_batch(self, batch, optimizer, epoch):
         self.model.train()
@@ -85,6 +89,10 @@ class Trainer:
 
     @staticmethod
     def prepare_dataloaders(train_dataset, eval_dataset):
-        train_dataloader = DataLoader(train_dataset, batch_size=CONFIG["train_batch_size"], shuffle=True)
-        eval_dataloader = DataLoader(eval_dataset, batch_size=CONFIG["train_batch_size"], shuffle=True)
+        train_dataloader = DataLoader(
+            train_dataset, batch_size=CONFIG["train_batch_size"], shuffle=True
+        )
+        eval_dataloader = DataLoader(
+            eval_dataset, batch_size=CONFIG["train_batch_size"], shuffle=True
+        )
         return train_dataloader, eval_dataloader
