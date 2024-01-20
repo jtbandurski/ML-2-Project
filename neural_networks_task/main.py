3a61fcb162b5ca63dea70d901e102b860f3e9057
Dominik Koterwa <koterwadominik@icloud.com>
2024-01-20 16:57:42 +0100

    prepare more or less final version of pipeline

--- neural_networks_task/main.py
+++ neural_networks_task/main.py
@@ -1,6 +1,6 @@
 from model import MobileNetV3
 import torch
-from utils import CONFIG
+from utils import CONFIG, DEVICE
 from torch.optim import Adam
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,37 +8,43 @@ import pandas as pd
 from trainer import Trainer
 from datasets import BirdCLEFDataset
 from argparse import ArgumentParser
+import os
+import numpy as np
 
 if __name__ == "__main__":
     parser = ArgumentParser()
+    parser.add_argument("--model_name", type=str)
     parser.add_argument("--overfit_single_batch", type=bool, default=False)
+    parser.add_argument("--save_results", type=bool, default=False)
+    parser.add_argument("--save_plots", type=bool, default=False)
     args = parser.parse_args()
 
     df = pd.read_csv("data/train_metadata.csv")
     df = df.groupby("common_name").filter(lambda x: len(x) >= 5)
-    print(df.head())
+    df = df.iloc[:100]
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = MobileNetV3().to(device)
+    model = MobileNetV3()
     full_dataset = BirdCLEFDataset(
         df, target_sample_rate=CONFIG["sample_rate"], max_time=CONFIG["max_time"]
     )
     train_dataset, eval_dataset = full_dataset.train_test_split(stratify=True)
-
     train_dataset.encode_labels()
     eval_dataset.encode_labels()
 
-    optimizer = Adam(model.parameters(), lr=1e-4)
+    optimizer = Adam(model.parameters(), lr=CONFIG["learning_rate"])
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, eta_min=1e-5, T_max=10)
-    trainer = Trainer(optimizer, model, device)
+    trainer = Trainer(
+        model=model, optimizer=optimizer, scheduler=scheduler, model_name=args.model_name
+    )
+    trainer.initialize_metrics_dict()
     train_dataloader, eval_dataloader = trainer.prepare_dataloaders(train_dataset, eval_dataset)
 
     best_eval_f1 = 0
     if args.overfit_single_batch:
-        train_mels = next(iter(train_dataloader))[0].to(device)
-        train_labels = next(iter(train_dataloader))[1].to(device)
-        eval_mels = next(iter(eval_dataloader))[0].to(device)
-        eval_labels = next(iter(eval_dataloader))[1].to(device)
+        train_mels = next(iter(train_dataloader))[0].to(DEVICE)
+        train_labels = next(iter(train_dataloader))[1].to(DEVICE)
+        eval_mels = next(iter(eval_dataloader))[0].to(DEVICE)
+        eval_labels = next(iter(eval_dataloader))[1].to(DEVICE)
 
         single_batch = {
             "train_mels": train_mels,
@@ -50,26 +56,27 @@ if __name__ == "__main__":
         for epoch in range(CONFIG["epochs"]):
             trainer.overfit_single_batch(
                 batch=single_batch,
-                optimizer=optimizer,
                 epoch=epoch,
             )
     else:
+        os.makedirs(CONFIG["MODELS_DIR"], exist_ok=True)
         for epoch in range(CONFIG["epochs"]):
-            train_loss, train_accuracy, train_f1 = trainer.train(
-                train_dataloader, optimizer, scheduler, device, epoch
-            )
-            valid_loss, eval_accuracy, eval_f1 = trainer.eval(
-                eval_dataloader, device, epoch, eval_one_batch=False
-            )
-            print(
-                f"Statistics for epoch {epoch+1}: \n \
-                Train Accuracy: {train_accuracy} \n \
-                Train F1: {train_f1} \n \
-                Eval Accuracy: {eval_accuracy} \n \
-                Eval F1: {eval_f1}"
-            )
-            if eval_f1 > best_eval_f1:
-                print(f"Validation F1 Improved - {best_eval_f1} ---> {eval_f1}")
-                torch.save(model.state_dict(), f"./model_0.bin")
-                print(f"Saved model checkpoint at ./model_0.bin")
-                best_eval_f1 = eval_f1
+            training_metrics = trainer.train(dataloader=train_dataloader, epoch=epoch)
+            eval_metrics = trainer.eval(dataloader=eval_dataloader, epoch=epoch)
+            trainer.update_metrics_state(epoch, training_metrics, eval_metrics)
+
+            if eval_metrics["weighted_metrics"]["f1"] > best_eval_f1:
+                print(
+                    f"Evaluation F1 Improved - {np.round(best_eval_f1, 2)} ---> {np.round(eval_metrics['weighted_metrics']['f1'], 2)}"
+                )
+                torch.save(
+                    model.state_dict(),
+                    f"{CONFIG['MODELS_DIR']}/{'_'.join(trainer.model_name.split(' '))}.bin",
+                )
+                print(f"Saved model checkpoint")
+                best_eval_f1 = eval_metrics["weighted_metrics"]["f1"]
+
+        if args.save_results:
+            trainer.save_results_to_csv()
+        if args.save_plots:
+            trainer.prepare_and_save_plots()
