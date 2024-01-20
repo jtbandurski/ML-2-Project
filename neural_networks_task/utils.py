5c871f39540f4088c106af2eebedd5195dcb85f7
Dominik Koterwa <koterwadominik@icloud.com>
2024-01-20 16:57:42 +0100

    prepare more or less final version of pipeline

--- neural_networks_task/utils.py
+++ neural_networks_task/utils.py
@@ -1,7 +1,8 @@
 import torch.nn as nn
+import torch
 
 CONFIG = {
-    "epochs": 100,
+    "epochs": 30,
     "num_classes": 264,
     "train_batch_size": 16,
     "eval_batch_size": 16,
@@ -12,7 +13,13 @@ CONFIG = {
     "max_time": 5,
     "n_mels": 224,
     "n_fft": 1024,
+    "learning_rate": 1e-5,
+    "PLOTS_DIR": "./plots/results",
+    "RESULTS_DIR": "./results",
+    "MODELS_DIR": "./saved_models",
 }
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def loss_fn(outputs, labels):
